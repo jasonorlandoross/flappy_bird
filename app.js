e2235d399b9a4ffa7a2d00dc788015c13970f6a9
Jason Ross <jasonorlandoross@gmail.com>
2022-09-17 13:52:58 -0500

    flappy bird is flappin

--- app.js
+++ app.js
@@ -1,48 +1,68 @@
 
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = 0;
-var counter = 0;
-setTimeout(
-hole.addEventListener('animationiteration', () => {
-var random = Math.random()*3;
-var top = (random*100)+150;
-hole.style.top = -(top) + "px";
-counter++;
-}),10000);
-setInterval(function(){
-var characterTop = 
-parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-if(jumping==0){
-character.style.top = (characterTop+3)+"px";
-}
-var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-var characterTop = 
-parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-var cTop = -(500-characterTop);
-if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
-    alert("Game Over. Score: "+counter);
-    character.style.top = 100 + "px";
-    counter=0;
-}
-},10);
+const block = document.getElementById("block");
+const hole = document.getElementById("hole");
+const character = document.getElementById("character");
+const startButton = document.getElementById("start-button");
+const gameBoard = document.querySelector("#game");
+let jumping = 0;
+let counter = 0;
 
-function jump(){ 
-jumping = 1;
-let jumpCount = 0;
-var jumpInterval = setInterval(function(){
-    var characterTop =
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if((characterTop>6)&&(jumpCount<15)) {
-     character.style.top = (characterTop-5)+"px";
+// making the animation run a different hole placement every animation 
+
+const buildWalls = () => setTimeout(
+    hole.addEventListener('animationiteration', () => {
+        const random = Math.random() * 3;
+        const top = (random * 100) + 150;
+        hole.style.top = -(top) + "px";
+        counter++;
+    }), 10000);
+
+// creating "gravity" so that character will fall if not being "clicked/jumping"
+const setGravity = () => setInterval(function () {
+    const characterTop =
+        parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if (jumping == 0) {
+        character.style.top = (characterTop + 3) + "px";
     }
-    if(jumpCount>20){
-        clearInterval(jumpInterval);
-        jumping=0;
-        jumpCount=0;
+
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    // var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    const cTop = -(500 - characterTop);
+
+    // if character hits a block then its game over but if you hit a block and a hole is there then your good 
+    // if you hit the bottom then its game over 
+
+    if ((characterTop > 480) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
+        alert("Game Over. Score: " + counter);
+        character.style.top = 100 + "px";
+        counter = 0;
     }
-    jumpCount++;
 }, 10);
+
+// making the jumping function, allowing gravity to stop if the character is jumping
+function jump() {
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function () {
+        var characterTop =
+            parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        //making sure the character cant jump all the above the blocks 
+        if ((characterTop > 6) && (jumpCount < 15)) {
+            character.style.top = (characterTop - 5) + "px";
+        }
+        if (jumpCount > 20) {
+            clearInterval(jumpInterval);
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    }, 10);
 }
+
+startButton.addEventListener('click', () => {
+    gameBoard.style.display = "block";
+    buildWalls()
+    setGravity()
+})
+
